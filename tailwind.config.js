4d1df3e550da2b3c4c1cfccf6f5696fa02ef3841
sggyflt <sgflt88@gmail.com>
2024-01-26 19:48:58 +0100

    feat: add custom tw-colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,35 @@ module.exports = {
   content: ["./dist/*.html"],
   theme: {
     extend: {},
+    colors: {
+      neutral: {
+        100: "#FFFFFF",
+        200: "#F6F7F8",
+        300: "#ECEEF0",
+        400: "#BDC4C7",
+        500: "#3F484D",
+        600: "#222200",
+      },
+      blue: {
+        100: "#EBF8FF",
+        200: "#D9F2FF",
+        300: "#0078B2",
+        400: "#006FA5",
+        500: "#006799",
+      },
+      emerald: {
+        100: "#57D9B9",
+        200: "#FFDF80",
+        300: "#1DBF97",
+      },
+      gold: {
+        100: "#FFD559",
+        200: "#FFDF80",
+      },
+      red: {
+        100: "#BF434D",
+      },
+    },
   },
   plugins: [],
 };
